538ecab477647dd7e87f83ed1039a8dfb7b6fa46
Jaime Pillora <dev@jpillora.com>
2018-09-03 21:28:48 +1000

    moved in config var templating

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const init = () => {
     try {
       configs = require(filepath);
     } catch (err) {
-      console.log("invalid config.json: " + err);
+      console.log("invalid config.json:", err.stack);
       process.exit(1);
     }
   }
@@ -34,8 +34,49 @@ const get = (key, def) => {
   return value;
 };
 
+//create a new destination object using the
+//provided source object and specification
+const template = (src, spec) => {
+  if (typeof src !== "object") {
+    throw `expected source object`;
+  } else if (typeof spec !== "object") {
+    throw `expected spec object`;
+  }
+  const dst = {};
+  for (let k in spec) {
+    let val = src[k];
+    const vtype = typeof val;
+    const field = spec[k];
+    const ftype = field.type;
+    if (!ftype) {
+      throw `specification field "${k}" missing type`;
+    } else if (ftype === "config") {
+      if (val && exports.has(val)) {
+        val = exports.get(val);
+      } else if (!val && field.key && exports.has(field.key)) {
+        val = exports.get(field.key);
+      }
+    } else if (ftype === "array" && Array.isArray(val) && field.element) {
+      val = val.map(v => template(v, field.element));
+    } else if (vtype === "undefined") {
+      if (field.required) {
+        throw `field "${k}" is required`;
+      } else if (field.default) {
+        val = field.default;
+      }
+    } else if (ftype !== vtype) {
+      throw `field "${k}" is ${vtype} (expected ${ftype})`;
+    }
+    if (val !== undefined) {
+      dst[k] = val;
+    }
+  }
+  return dst;
+};
+
 var exports = get;
 exports.dataPath = dataPath;
 exports.has = has;
 exports.get = get;
+exports.template = template;
 module.exports = exports;
